2716d8f248e5c65a11d2a8bff6ed12eb7be1638c
jjkip <jjlangatjose@gmail.com>
2023-06-05 11:31:33 +0300

    Initial Commit

---
+++ InteractiveHeart/InteractiveHeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveHeartApp.swift
+//  InteractiveHeart
+//
+//  Created by Joseph Langat on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveHeartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
