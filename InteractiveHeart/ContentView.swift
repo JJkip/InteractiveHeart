a698d5c67f89fb9c4a78deaca5d79c3471f5f7b2
jjkip <jjlangatjose@gmail.com>
2023-06-05 13:11:58 +0300

    Initital commit heart drawn

--- InteractiveHeart/ContentView.swift
+++ InteractiveHeart/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var colorChange = false
+    @State private var sizeChange = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image(systemName: "heart.fill")
+            .font(.system(size: 200))
+            .foregroundColor(colorChange ? .yellow : .red)
+            .scaleEffect(sizeChange ? 1.5 : 1)
+            .animation(.default)
+            .onTapGesture {
+                self.colorChange.toggle()
+            }
     }
 }
 
